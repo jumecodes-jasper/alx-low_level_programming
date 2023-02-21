71489481e8d72d49a9546abe532211c9214e73a8
jumecodes-jasper <jahbuike.ugo@gmail.com>
2023-02-20 18:21:10 -0800

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-/** Main ~ entry
- * return : Always 0
+/**
+ *  main -Entry point
+ *
+ *  Return: Always 0 (Success)
  */
-
 int main(void)
 {
-
-	puts("\"Programming is like a multilingual puzzle");
-
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
