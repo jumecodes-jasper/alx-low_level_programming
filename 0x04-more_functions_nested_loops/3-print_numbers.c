23c738d79d1aacc28c214c9e3cd9db72e039b154
jumecodes-jasper <jahbuike.ugo@gmail.com>
2023-02-25 10:05:47 -0800

    task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
-*print_numbers - print 0123456789
-*
+*print_numbers - print 0-9
 *Return: void
 */
 
@@ -12,7 +11,7 @@ void print_numbers(void)
 
 	for (c = '0'; c <= '9'; c++)
 	{
-	_putchar(c);
+		 _putchar(c);
 	}
 	_putchar('\n');
 }
