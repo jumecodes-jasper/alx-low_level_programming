1907361183d0360eab6e0545ed51cd64d2ffb402
jumecodes-jasper <jahbuike.ugo@gmail.com>
2023-04-11 04:05:47 -0700

    task 5

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * flip_bits - it flips to get from one number to another.
+ * @n: is the first number
+ * @m: is the second number
+ * Return the number of bits you would need to flip
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int diff, res;
+	unsigned int a, i;
+
+	a = 0;
+	res = 1;
+	diff = n ^ m;
+	for (i = 0; i < (sizeof(unsigned long int) * 8); i++)
+	{
+		if (result == (diff & res))
+			a++;
+		result <<= 1;
+	}
+
+	return (a);
+}
